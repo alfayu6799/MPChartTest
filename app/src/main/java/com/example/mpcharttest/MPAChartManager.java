0ceca0a2053c369ff89caa8b32fe16e03f002a1c
Jerry <lala@yhcyhc.com>
2021-03-10 15:56:47 +0800

    混合圖

--- app/src/main/java/com/example/mpcharttest/MPAChartManager.java
+++ app/src/main/java/com/example/mpcharttest/MPAChartManager.java
@@ -1,14 +1,22 @@
 package com.example.mpcharttest;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 
 import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MPAChartManager {
@@ -20,7 +28,6 @@ public class MPAChartManager {
     private YAxis leftYAxis;            //左侧Y轴
     private YAxis rightYAxis;           //右侧Y轴 自定义XY轴值
 
-    private double degree;
 
     //建構子
     public MPAChartManager(Context context, CombinedChart combinedChart) {
@@ -44,6 +51,8 @@ public class MPAChartManager {
         combinedChart.getDescription().setEnabled(false);
         //不用圖例
         combinedChart.getLegend().setEnabled(false);
+
+        combinedChart.setDrawOrder(new CombinedChart.DrawOrder[]{CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE});
 
         //是否繪製X軸網格線
         xAxis.setDrawGridLines(true);
@@ -74,31 +83,90 @@ public class MPAChartManager {
         //繪製X軸線
         xAxis.setDrawAxisLine(false);
 
-//        CombinedData data = new CombinedData();
-//
-//        data.setData(generateLineData());
-//        data.setData(generateBarData());
-//
-//        combinedChart.setData(data);
-//        combinedChart.invalidate();
-
-    }
-
-    private BarData generateBarData() {
-        return null;
-    }
-
-    private LineData generateLineData() {
-        return null;
     }
 
     public void showCombinedChart(List<CycleRecord.SuccessBean> dataList) {
         CombinedData combinedData = new CombinedData();
 
-        for (int i = 0 ; i < dataList.size(); i++){
-            degree = dataList.get(i).getTemperature(); //體溫
+        combinedData.setData(getLineDate(dataList));
+        combinedData.setData(getBarDate(dataList));
+
+        combinedChart.setData(combinedData);
+        combinedChart.invalidate();
+    }
+
+    private BarData getBarDate(List<CycleRecord.SuccessBean> dataList) {
+        BarData barData = new BarData();
+
+        List<BarEntry> entries1 = new ArrayList<BarEntry>();
+        List<BarEntry> entries2 = new ArrayList<BarEntry>();
+
+        for(int i = 0; i < dataList.size(); i++){
+            CycleRecord.SuccessBean data = dataList.get(i);
+            if (data.getCycleStatus().contains(4)){
+                data.setTemperature(40);
+                entries1.add(new BarEntry(i, (float) data.getTemperature()));
+            }else if (data.getCycleStatus().contains(6)){
+                data.setTemperature(40);
+                entries2.add(new BarEntry(i, (float) data.getTemperature()));
+            }
 
         }
 
+        BarDataSet barDataSet = new BarDataSet(entries1, "經期"); // add entries to dataset
+        barDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        BarDataSet barDataSet2 = new BarDataSet(entries2, "排卵期"); // add entries to dataset
+        barDataSet2.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        //设置数据显示颜色：柱子颜色
+        barDataSet.setColor(Color.rgb(225,63,174));
+        barDataSet2.setColor(Color.rgb(225,186,63));
+
+        //不顯示數據點數值
+        barDataSet.setDrawValues(false);
+        barDataSet2.setDrawValues(false);
+
+        barData.addDataSet(barDataSet);
+        barData.addDataSet(barDataSet2);
+
+        barData.setBarWidth(1.2f);
+
+        return barData;
+    }
+
+    private LineData getLineDate(List<CycleRecord.SuccessBean> dataList) {
+        LineData lineData = new LineData();
+
+        ArrayList<String> label = new ArrayList<>();
+
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        for (int i = 0; i < dataList.size(); i++){
+            double degree = dataList.get(i).getTemperature();
+            String[] str = dataList.get(i).getTestDate().split("-");
+            String testDay = str[2];
+
+            if (degree > 0)
+            entries.add(new Entry(i, (float) degree));
+
+            label.add(testDay);
+        }
+
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(label));
+        xAxis.setLabelCount(label.size());
+        xAxis.setLabelCount(10);
+
+        LineDataSet set = new LineDataSet(entries, "");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(Color.BLACK);
+        set.setLineWidth(0.9f);
+        set.setCircleColor(Color.BLUE);
+        set.setCircleRadius(2f);
+        set.setMode(LineDataSet.Mode.LINEAR);
+        set.setDrawValues(false); //是否顯示圓點的數值
+        lineData.addDataSet(set);
+
+        return lineData;
     }
 }
--- app/src/main/java/com/example/mpcharttest/MainActivity.java
+++ app/src/main/java/com/example/mpcharttest/MainActivity.java
@@ -28,131 +28,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = "MainActivity";
-
     private CombinedChart combinedChart;
-
-    private CycleRecord cycleRecord;
-    private List<CycleRecord.SuccessBean> dataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
-
+        combinedChart = findViewById(R.id.chart);
+        
         initData();
-
     }
 
     private void initData() {
-        String myJsonStr = loadJSONFromAsset("menstruation_03.json");
-        cycleRecord = CycleRecord.newInstance(myJsonStr);
-        dataList = cycleRecord.getSuccess();
+        String myJsonStr = loadJSONFromAsset("menstruation.json");
+//        String myJsonStr = loadJSONFromAsset("menstruation_03.json");  //全部沒有量測的json會閃退
+        CycleRecord cycleRecord = CycleRecord.newInstance(myJsonStr);
+        List<CycleRecord.SuccessBean> dataList = cycleRecord.getSuccess();
 
         MPAChartManager mpaChartManager = new MPAChartManager(this,combinedChart);
         mpaChartManager.showCombinedChart(dataList);
     }
-
-    private void initView() {
-        combinedChart = findViewById(R.id.chart);
-    }
-
-//    //折線圖
-//    private LineData generateLineData(List<CycleRecord.SuccessBean> dataList){
-//        LineData lineData = new LineData();
-//
-//        ArrayList<String> label = new ArrayList<>(); //日期
-//
-//        ArrayList<Entry> entries = new ArrayList<>();
-//
-//        String myJsonStr = loadJSONFromAsset("menstruation_03.json");
-//        cycleRecord = CycleRecord.newInstance(myJsonStr);
-//
-//        this.dataList = cycleRecord.getSuccess();
-//
-//        for (int i = 0; i < this.dataList.size(); i++){
-//
-//            CycleRecord.SuccessBean data = this.dataList.get(i);
-//
-//            String[] str = data.getTestDate().split("-");
-//            String testDay = str[2];
-//
-//            if (data.getTemperature() > 0 ){
-//                entries.add(new Entry(i, (float) data.getTemperature()));
-//            }
-//
-//            label.add(testDay);
-//        }
-//
-//        xAxis.setValueFormatter(new IndexAxisValueFormatter(label));
-//        xAxis.setLabelCount(label.size());
-//        xAxis.setLabelCount(10,true);
-//
-//        LineDataSet set = new LineDataSet(entries, "");
-//        set.setAxisDependency(YAxis.AxisDependency.LEFT);
-//        set.setColor(Color.BLACK);
-//        set.setLineWidth(0.9f);
-//        set.setCircleColor(Color.BLUE);
-//        set.setCircleRadius(2f);
-//        set.setMode(LineDataSet.Mode.LINEAR);
-//        set.setDrawValues(false); //是否顯示圓點的數值
-//        lineData.addDataSet(set);
-//
-//        return lineData;
-//    }
-//
-//    //長條圖
-//    private BarData generateBarData(List<CycleRecord.SuccessBean> dataList){
-//        BarData barData = new BarData();
-//
-//        List<BarEntry> entries1 = new ArrayList<BarEntry>();
-//        List<BarEntry> entries2 = new ArrayList<BarEntry>();
-//
-//        String myJsonStr = loadJSONFromAsset("menstruation.json");
-//        cycleRecord = CycleRecord.newInstance(myJsonStr);
-//
-//        this.dataList = cycleRecord.getSuccess();
-//
-//        for (int i = 0; i < this.dataList.size(); i++) {
-//
-//            CycleRecord.SuccessBean data = this.dataList.get(i);
-//            String[] str = data.getTestDate().split("-");
-//            String testDay = str[2];
-//
-//            if (data.getCycleStatus().contains(4)) {
-//                data.setTemperature(40);
-//                entries1.add(new BarEntry(i, (float) data.getTemperature()));
-//            } else if (data.getCycleStatus().contains(6)) {
-//                data.setTemperature(40);
-//                entries2.add(new BarEntry(i, (float) data.getTemperature()));
-//            }
-//
-//        }
-//
-//        BarDataSet barDataSet = new BarDataSet(entries1, "經期"); // add entries to dataset
-//        barDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-//
-//        BarDataSet barDataSet2 = new BarDataSet(entries2, "排卵期"); // add entries to dataset
-//        barDataSet2.setAxisDependency(YAxis.AxisDependency.LEFT);
-//
-//        //设置数据显示颜色：柱子颜色
-//        barDataSet.setColor(Color.rgb(225,63,174));
-//        barDataSet2.setColor(Color.rgb(225,186,63));
-//
-//        //不顯示數據點數值
-//        barDataSet.setDrawValues(false);
-//        barDataSet2.setDrawValues(false);
-//
-//        barData.addDataSet(barDataSet);
-//        barData.addDataSet(barDataSet2);
-//
-//        barData.setBarWidth(1.2f);
-//
-//        return barData;
-//    }
 
     //讀取local json file
     public String loadJSONFromAsset(String fileName)
